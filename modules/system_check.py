525eafefdc7d5e59e55c2348f456253a0a72da71
Jonathan Greene <jonathangreene1983@gmail.com>
2019-07-26 01:31:20 +0100

    added python modules

---
+++ modules/system_check.py
@@ -0,0 +1,48 @@
+import serial
+from time import sleep
+
+robot = serial.Serial('/dev/ttyUSB0', 9600)
+connected = False
+
+def checkSerialConnection(func):
+    def wrapper(*args, **kwargs):
+        verified = False
+        print("Verifying serial port...")
+        while verified is False:
+            if robot.in_waiting > 0:
+                verified = True
+            else:
+                verified = False
+        print("Serial port connected...")
+        func(*args, **kwargs)
+    return wrapper
+
+@checkSerialConnection        
+def check_commands(connection):
+    print("Trying forward motion...")
+    connection.write('forward')
+    sleep(2)
+    print("Halting...")
+    connection.write('halt')
+    sleep(2)
+    print("Trying reverse motion...")
+    connection.write('back')
+    sleep(2)
+    print("Halting...")
+    connection.write('halt')
+    sleep(2)
+    print("Trying left rotate...")
+    connection.write('left')
+    sleep(2)
+    print("Halting...")
+    connection.write('halt')
+    sleep(2)
+    print("Trying right rotate...")
+    connection.write('right')
+    sleep(2)
+    print("Halting...")
+    connection.write('halt')
+
+check_commands(robot)
+
+robot.close()
